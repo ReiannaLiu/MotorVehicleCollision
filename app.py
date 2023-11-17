4d9a3f28bc424518deddd8d6065249edfa322d34
Reianna <rl3176@barnard.edu>
2023-11-17 12:43:45 -0500

    update line chart + select

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from flask import Flask,jsonify
 from datetime import datetime
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 from flask import render_template
+from collections import defaultdict
+from datetime import datetime as dt
 
 #################################################
 # Database Setup
@@ -70,6 +72,42 @@ def read_motor_collision_by_zip(zip_code):
     result = motor_collision_schema.dump(all_collisions, many=True)
     return jsonify(result)
 
+@app.route("/api/v1.0/motor_collision/<zip_code>/by_weekday")
+def read_motor_collision_by_zip_by_weekday(zip_code):
+    # Query all records from the MotorCollision table
+    all_collisions = session.query(MotorCollision).filter(MotorCollision.zip_code == zip_code).all()
+
+    # Aggregate data by weekday
+    weekday_by_zip = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0}
+    for collision in all_collisions:
+        weekday_by_zip[collision.crash_day_of_week] += 1
+
+    return jsonify(weekday_by_zip)
+
+@app.route("/api/v1.0/motor_collision/<zip_code>/by_hour")
+def read_motor_collision_by_zip_by_hour(zip_code):
+    # Query all records from the MotorCollision table
+    all_collisions = session.query(MotorCollision).filter(MotorCollision.zip_code == zip_code).all()
+
+    # Aggregate data by hour
+    hour_by_zip = {hour: 0 for hour in range(24)}
+    for collision in all_collisions:
+        hour_by_zip[collision.crash_datetime.hour] += 1
+
+    return jsonify(hour_by_zip)
+
+@app.route("/api/v1.0/motor_collision/<zip_code>/by_month")
+def read_motor_collision_by_zip_by_month(zip_code):
+    # Query all records from the MotorCollision table
+    all_collisions = session.query(MotorCollision).filter(MotorCollision.zip_code == zip_code).all()
+
+    # Aggregate data by month
+    month_by_zip = {month: 0 for month in range(1,13)}
+    for collision in all_collisions:
+        month_by_zip[collision.crash_datetime.month] += 1
+
+    return jsonify(month_by_zip)
+
 @app.route("/api/v1.0/collision_by_zip")
 def read_collision_by_zip():
     # Query all records from the CollisionByZip table
@@ -78,7 +116,6 @@ def read_collision_by_zip():
     # Serialize the query results to JSON using the schema
     result = collision_by_zip_schema.dump(all_collisions, many=True)
     return jsonify(result)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
